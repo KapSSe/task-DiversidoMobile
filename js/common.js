9b5780176bf58ce1ac49c9fe290ce2c3d28b7802
KapSSegit config --global user.email kapranovstanislav@gmail.comgit helpgit config --global user.name KapSSe <kapranovstanislav@gmail.com>
2017-08-12 13:22:07 +0300

    'init'

---
+++ js/common.js
@@ -0,0 +1,51 @@
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+  
+
+ 	$('#toggleSide').on('click', function(){
+		$('.logo').toggleClass('logo' + '--collapsed').toggleClass('event');
+		$('.wrap-rightbar').toggleClass('wrap-rightbar' + '--collapsed');
+		$('.leftbar').toggleClass('leftbar' + '--collapsed');
+		$('.leftbar__user').toggleClass('leftbar__user' + '--collapsed');
+		$('.leftbar__info').toggleClass('leftbar__info' + '--collapsed');
+		$('.leftbar__header').toggleClass('leftbar__header' + '--collapsed');
+		$('.leftbar__menu').toggleClass('leftbar__menu' + '--collapsed');
+		$('.accordion').toggleClass('accordion' + '--collapsed');
+		$('.msgcount').toggleClass('msgcount' + '--collapsed');
+		$('.tablecontainer').toggleClass('tablecontainer' + '--collapsed');
+		if ($('.logo').is('.logo--collapsed, .event')){
+			$('.leftbar').on('mouseenter mouseleave', function(){
+				$('.logo').toggleClass('logo' + '--collapsed').removeClass('event');
+				$('.wrap-rightbar').toggleClass('wrap-rightbar' + '--collapsed');
+				$('.leftbar').toggleClass('leftbar' + '--collapsed');
+				$('.leftbar__user').toggleClass('leftbar__user' + '--collapsed');
+				$('.leftbar__info').toggleClass('leftbar__info' + '--collapsed');
+				$('.leftbar__header').toggleClass('leftbar__header' + '--collapsed');
+				$('.leftbar__menu').toggleClass('leftbar__menu' + '--collapsed');
+				$('.accordion').toggleClass('accordion' + '--collapsed');
+				$('.msgcount').toggleClass('msgcount' + '--collapsed');
+				$('.tablecontainer').toggleClass('tablecontainer' + '--collapsed');
+			})
+		}
+
+	})
+	 
+
+	
+	$('.menu-resp').on('click', function(){
+		$('.leftbar').toggleClass('leftbar--active');
+	})
